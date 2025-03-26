0145c0774894727b26e8cfd1367494d039fb239b
Hayden Lister <haydenglister@gmail.com>
2025-03-25 21:29:16 -0700

    Added user group to data classes, and allowed for conversion between camel and snake case elements

--- app/src/main/java/com/example/medimobile/data/remote/model.kt
+++ app/src/main/java/com/example/medimobile/data/remote/model.kt
@@ -1,5 +1,16 @@
 package com.example.medimobile.data.remote
 
+import com.google.gson.annotations.SerializedName
+
 // Data Classes for API
-data class LoginRequest(val username: String, val password: String)
-data class LoginResponse(val token: String)
+data class LoginRequest(
+    @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String,
+    @SerializedName("user_group") val userGroup: String
+)
+
+data class LoginResponse(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("token_type") val tokenType: String,
+    @SerializedName("user_role") val userRole: String
+)
