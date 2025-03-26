fb98e51bbba1a0783eaed68b041c94572ec99741
Hayden Lister <haydenglister@gmail.com>
2025-03-25 21:26:45 -0700

    Adjusted Authapi to work with new response data model

--- app/src/main/java/com/example/medimobile/data/remote/api.kt
+++ app/src/main/java/com/example/medimobile/data/remote/api.kt
@@ -1,10 +1,11 @@
 package com.example.medimobile.data.remote
 
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 // Retrofit API Interface
 interface AuthApi {
-    @POST("login")
-    suspend fun login(@Body credentials: LoginRequest): LoginResponse
+    @POST("api/login")
+    suspend fun login(@Body loginRequest: LoginRequest): Response<LoginResponse>
 }
