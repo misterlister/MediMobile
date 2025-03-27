a970825b0c316069b70acb3ad37f8ce2d5991b15
Hayden Lister <haydenglister@gmail.com>
2025-03-27 16:27:08 -0700

    Added SerializedNames to Patient Encounter to convert between snake and camel case

--- app/src/main/java/com/example/medimobile/data/model/PatientEncounter.kt
+++ app/src/main/java/com/example/medimobile/data/model/PatientEncounter.kt
@@ -1,29 +1,46 @@
 package com.example.medimobile.data.model
 
+import com.google.gson.annotations.SerializedName
 import java.time.LocalDate
 import java.time.LocalTime
 
 data class PatientEncounter (
     val age: Int = 0,
+    @SerializedName("arrival_method")
     val arrivalMethod: String = "",
-    val arrivalDate: LocalDate = LocalDate.now(),
-    val arrivalTime: LocalTime = LocalTime.now(),
+    @SerializedName("arrival_date")
+    val arrivalDate: LocalDate? = null,
+    @SerializedName("arrival_time")
+    val arrivalTime: LocalTime? = null,
+    @SerializedName("chief_complaint")
     val chiefComplaint: String = "",
     val comment: String = "",
-    val departureDate: LocalDate = LocalDate.now(),
-    val departureTime: LocalTime = LocalTime.now(),
+    @SerializedName("departure_date")
+    val departureDate: LocalDate? = null,
+    @SerializedName("departure_time")
+    val departureTime: LocalTime? = null,
+    @SerializedName("departure_dest")
     val departureDest: String = "",
+    @SerializedName("document_num")
     val documentNum: String = "",
     val location: String = "",
     val role: String = "",
+    @SerializedName("qr_code")
     val visitId: String = "",
+    @SerializedName("triage_acuity")
     val triageAcuity: String = "",
+    @SerializedName("discharge_diagnosis")
     val dischargeDiagnosis: String = "",
+    @SerializedName("patient_encounter_uuid")
     val dbKey: String = "",
     val triageStatus: StageStatus = StageStatus.NOT_STARTED,
     val informationCollectionStatus: StageStatus = StageStatus.NOT_STARTED,
     val dischargeStatus: StageStatus = StageStatus.NOT_STARTED,
-    val complete: Boolean = false
+    val complete: Boolean = false,
+    @SerializedName("user_uuid")
+    val userUuid: String = "",
+    @SerializedName("user_email")
+    val userEmail: String = ""
 )
 
 enum class StageStatus {
