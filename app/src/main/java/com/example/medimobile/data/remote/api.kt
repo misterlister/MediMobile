24c1dd8fdd3e143d763b16447b272a11345f9c04
Hayden Lister <haydenglister@gmail.com>
2025-03-27 16:24:41 -0700

    Added API interface for getting patient encounters from the database

--- app/src/main/java/com/example/medimobile/data/remote/api.kt
+++ app/src/main/java/com/example/medimobile/data/remote/api.kt
@@ -1,11 +1,25 @@
 package com.example.medimobile.data.remote
 
+import com.example.medimobile.data.model.PatientEncounter
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 // Retrofit API Interface
 interface AuthApi {
     @POST("api/login")
     suspend fun login(@Body loginRequest: LoginRequest): Response<LoginResponse>
+}
+
+interface GetEncountersApi {
+    @GET("api/medical/forms")
+    suspend fun getPatientEncounters(
+        @Query("user_uuid") userUuid: String? = null,
+        @Query("arrival_date_min") arrivalDateMin: String? = null,
+        @Query("arrival_date_max") arrivalDateMax: String? = null,
+        @Header("Authorization") token: String
+    ): Response<List<PatientEncounter>>
 }
