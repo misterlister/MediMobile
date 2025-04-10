d26a001706d528538e2f56b9d07d6f3fc1e1a1d7
Hayden Lister <haydenglister@gmail.com>
2025-04-10 15:22:28 -0700

    Created LoadingIndicator composable to grey out and lock the screen, and display a loading circle whenever the database is being accessed.

---
+++ app/src/main/java/com/example/medimobile/ui/components/LoadingIndicator.kt
@@ -0,0 +1,31 @@
+package com.example.medimobile.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+
+@Composable
+fun LoadingIndicator(isLoading: Boolean) {
+    if (isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                // Set the background color to semi-transparent black
+                .background(Color(0x80000000))
+                .pointerInput(Unit) {
+                    // Block user interaction while loading
+                    detectTapGestures()
+                }
+        ) {
+            // Align the progress indicator in the center of the Box
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
