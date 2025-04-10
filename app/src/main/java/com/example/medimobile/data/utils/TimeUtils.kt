934cbb12021d9257bc181f40647ab105ee4a0706
Hayden Lister <haydenglister@gmail.com>
2025-04-10 15:24:57 -0700

    Switched time and date formatting for encounter table

--- app/src/main/java/com/example/medimobile/data/utils/TimeUtils.kt
+++ app/src/main/java/com/example/medimobile/data/utils/TimeUtils.kt
@@ -6,6 +6,7 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneId
+import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 // Formatter for HH:mm
@@ -14,13 +15,13 @@ private val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
 // Formatter for dd/MM/yy
 val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yy")
 
-// Formatter for HH:mm - dd/MM/yy
+// Formatter for dd/MM/yy - HH:mm
 fun formatArrivalDateTime(encounter: PatientEncounter): String {
     // Handle null values for date and time with "Not Set"
     val datePart = encounter.arrivalDate?.format(dateFormatter) ?: NOT_SET
     val timePart = encounter.arrivalTime?.format(timeFormatter) ?: NOT_SET
 
-    return "$timePart - $datePart"
+    return "$datePart - $timePart"
 }
 
 // Convert local date and time to UTC string
@@ -30,4 +31,20 @@ fun convertToUTCString(date: LocalDate?, time: LocalTime?): String {
     val dateTime = LocalDateTime.of(date, time)
     val instant = dateTime.atZone(zoneId).toInstant()
     return DateTimeFormatter.ISO_INSTANT.format(instant) // Format as "2025-04-01T23:00:08.273Z"
+}
+
+fun convertToUTCDateString(date: LocalDate?): String? {
+    return date?.atStartOfDay(ZoneId.systemDefault())
+        ?.withZoneSameInstant(ZoneOffset.UTC)
+        ?.toLocalDateTime()
+        ?.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+}
+
+fun convertToUTCDateTimeString(date: LocalDate?, time: LocalTime?): String? {
+    if (date == null || time == null) return null
+    return LocalDateTime.of(date, time)
+        .atZone(ZoneId.systemDefault())
+        .withZoneSameInstant(ZoneOffset.UTC)
+        .toLocalDateTime()
+        .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
 }
