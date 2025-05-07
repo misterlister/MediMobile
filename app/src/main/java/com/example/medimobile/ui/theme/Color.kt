90045ddd3843ca2eedcab192b0a3633562296679
Hayden Lister <haydenglister@gmail.com>
2025-05-07 13:32:09 -0700

    Added constants for app-specific colours

--- app/src/main/java/com/example/medimobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/medimobile/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.example.medimobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+/*
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+*/
+
+val MediBlue = Color(0xFF1E80D9) // Blue colour to match Mediform's colour theme
+val MediRed = Color(0xFFC51B1B) // Red colour to represent cancellation and errors
+val MediGreen = Color(0xFF0CA60C) // Green colour to represent confirmation
+val MediYellow = Color(0xFFFFCC00) // Yellow colour to represent incomplete sections
+val LightBackground = Color(0xFFFFFFFF) // White
+val DarkBackground = Color(0xFF121212)  // Very dark gray
+val DarkBanner = Color(0xFF3B4D65) // Dark blue/grey for dark mode banners
+val LightBanner = Color(0xFF5E7B98) // Light blue/grey for light mode banners
+val InactiveColour = Color(0xFF707070) // Grey colour for inactive buttons/menus
+val TextOnBlue = Color.White // Text on a blue background
+val TextOnLight = Color.Black // Text on a light background
+val TextOnDark = Color.White // For text on a dark background
