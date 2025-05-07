e5f631f77b0672fb8ccc21ca7560a5f1cb3eb4f2
Hayden Lister <haydenglister@gmail.com>
2025-05-07 15:27:48 -0700

    Modified MediButton to allow for colour specification

--- app/src/main/java/com/example/medimobile/ui/components/templates/MediButton.kt
+++ app/src/main/java/com/example/medimobile/ui/components/templates/MediButton.kt
@@ -5,24 +5,23 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.medimobile.ui.theme.MediBlue
-import com.example.medimobile.ui.theme.TextOnBlue
+import com.example.medimobile.ui.theme.ButtonStatus
 
 @Composable
 fun MediButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
+    status: ButtonStatus = ButtonStatus.DEFAULT,
     content: @Composable RowScope.() -> Unit,
-    
 ) {
     Button(
         onClick = onClick,
         modifier = modifier,
         enabled = enabled,
         colors = ButtonDefaults.buttonColors(
-            containerColor = MediBlue,
-            contentColor = TextOnBlue
+            containerColor = status.backgroundColor,
+            contentColor = status.contentColor
         ),
         content = content
     )
