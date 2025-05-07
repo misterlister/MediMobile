df42fb19b43d237c844f07e7433612040204911a
Hayden Lister <haydenglister@gmail.com>
2025-05-07 15:25:15 -0700

    Added text colours that pair with each colour

--- app/src/main/java/com/example/medimobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/medimobile/ui/theme/Color.kt
@@ -14,13 +14,18 @@ val Pink40 = Color(0xFF7D5260)
 
 val MediBlue = Color(0xFF1E80D9) // Blue colour to match Mediform's colour theme
 val MediRed = Color(0xFFC51B1B) // Red colour to represent cancellation and errors
-val MediGreen = Color(0xFF0CA60C) // Green colour to represent confirmation
-val MediYellow = Color(0xFFFFCC00) // Yellow colour to represent incomplete sections
+val MediGreen = Color(0xFF00B700) // Green colour to represent confirmation
+val MediYellow = Color(0xFFFFD42C) // Yellow colour to represent incomplete sections
+val MediGrey = Color(0xFF8F8F8F) // Grey colour for inactive buttons/menus
 val LightBackground = Color(0xFFFFFFFF) // White
 val DarkBackground = Color(0xFF121212)  // Very dark gray
 val DarkBanner = Color(0xFF3B4D65) // Dark blue/grey for dark mode banners
 val LightBanner = Color(0xFF5E7B98) // Light blue/grey for light mode banners
-val InactiveColour = Color(0xFF707070) // Grey colour for inactive buttons/menus
+
 val TextOnBlue = Color.White // Text on a blue background
 val TextOnLight = Color.Black // Text on a light background
 val TextOnDark = Color.White // For text on a dark background
+val TextOnGrey = Color.White // Text on a grey background
+val TextOnRed = Color.White // Text on a red background
+val TextOnGreen = Color.White // Text on a green background
+val TextOnYellow = Color.Black // Text on a yellow background
