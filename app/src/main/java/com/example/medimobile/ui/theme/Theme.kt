25e88818c5bde065ee37063c6e2d96fb92638d93
Hayden Lister <haydenglister@gmail.com>
2025-05-08 01:27:01 -0700

    Added shadows to headings

--- app/src/main/java/com/example/medimobile/ui/theme/Theme.kt
+++ app/src/main/java/com/example/medimobile/ui/theme/Theme.kt
@@ -5,7 +5,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -45,23 +47,38 @@ private val LightColorScheme = lightColorScheme(
 
 // Custom TextStyle for app title text
 val appTitleTextStyle = TextStyle(
-    fontWeight = FontWeight.Bold, // Bold text
-    fontSize = 44.sp, // Font size
-    textAlign = TextAlign.Center // Center text alignment
+    fontWeight = FontWeight.Bold,
+    fontSize = 44.sp,
+    textAlign = TextAlign.Center,
+    shadow = Shadow(
+        color = Color.Black.copy(alpha = 0.5f),
+        offset = Offset(4f, 4f),
+        blurRadius = 7f
+    )
 )
 
 // Custom TextStyle for section title text
 val sectionTitleTextStyle = TextStyle(
-    fontWeight = FontWeight.Bold, // Bold text
-    fontSize = 24.sp, // Font size
-    textAlign = TextAlign.Center // Center text alignment
+    fontWeight = FontWeight.Bold,
+    fontSize = 24.sp,
+    textAlign = TextAlign.Center,
+    shadow = Shadow(
+        color = Color.Black.copy(alpha = 0.3f),
+        offset = Offset(2f, 2f),
+        blurRadius = 5f
+    )
 )
 
 // Custom TextStyle for screen title text
 val screenTitleTextStyle = TextStyle(
-    fontWeight = FontWeight.Bold, // Bold text
-    fontSize = 36.sp, // Font size
-    textAlign = TextAlign.Center // Center text alignment
+    fontWeight = FontWeight.Bold,
+    fontSize = 36.sp,
+    textAlign = TextAlign.Center,
+    shadow = Shadow(
+        color = Color.Black.copy(alpha = 0.4f),
+        offset = Offset(3f, 3f),
+        blurRadius = 6f
+    )
 )
 
 // Custom TextStyle for username and DocID text
