269be42633c7433ade21f20b4b8964342da44092
Hayden Lister <haydenglister@gmail.com>
2025-05-08 01:27:10 -0700

    Added shadows to buttons

--- app/src/main/java/com/example/medimobile/ui/components/templates/MediButton.kt
+++ app/src/main/java/com/example/medimobile/ui/components/templates/MediButton.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.medimobile.ui.theme.ButtonStatus
 
 @Composable
@@ -19,6 +20,11 @@ fun MediButton(
         onClick = onClick,
         modifier = modifier,
         enabled = enabled,
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 8.dp,
+            pressedElevation = 4.dp,
+            disabledElevation = 0.dp
+        ),
         colors = ButtonDefaults.buttonColors(
             containerColor = status.backgroundColor,
             contentColor = status.contentColor
