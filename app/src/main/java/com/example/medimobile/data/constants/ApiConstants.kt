2cd4aa55cffcc8ea6ed93bbce3711aa3f7250d91
Hayden Lister <haydenglister@gmail.com>
2025-05-07 18:42:21 -0700

    Updated API address to production database

---
+++ app/src/main/java/com/example/medimobile/data/constants/ApiConstants.kt
@@ -0,0 +1,8 @@
+package com.example.medimobile.data.constants
+
+object ApiConstants {
+    const val BASE_URL = "https://cloudrun-service-api-fuau5e2ywq-pd.a.run.app/"
+
+}
+// HOME: "http://192.168.1.108:5000/"
+// PROD: "https://cloudrun-service-api-fuau5e2ywq-pd.a.run.app/"
--- app/src/main/java/com/example/medimobile/data/remote/ApiConstants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.medimobile.data.remote
-
-object ApiConstants {
-    const val BASE_URL = "http://172.30.112.1:5000/"
-}
