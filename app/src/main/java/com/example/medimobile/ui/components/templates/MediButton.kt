80e9916d48272cc11b14a5caedcbed15c7267e62
Hayden Lister <haydenglister@gmail.com>
2025-05-21 19:18:58 -0700

    Enabled button highlighting, adjusted corner radius

--- app/src/main/java/com/example/medimobile/ui/components/templates/MediButton.kt
+++ app/src/main/java/com/example/medimobile/ui/components/templates/MediButton.kt
@@ -1,8 +1,11 @@
 package com.example.medimobile.ui.components.templates
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -11,24 +14,30 @@ import com.example.medimobile.ui.theme.ButtonStatus
 @Composable
 fun MediButton(
     onClick: () -> Unit,
-    modifier: Modifier = Modifier,
     enabled: Boolean = true,
     status: ButtonStatus = ButtonStatus.DEFAULT,
+    emptyHighlight: Boolean = false,
+    modifier: Modifier = Modifier,
     content: @Composable RowScope.() -> Unit,
 ) {
+    val shape = RoundedCornerShape(4.dp)
+
     Button(
         onClick = onClick,
-        modifier = modifier,
         enabled = enabled,
         elevation = ButtonDefaults.buttonElevation(
             defaultElevation = 8.dp,
-            pressedElevation = 4.dp,
+            pressedElevation = 2.dp,
             disabledElevation = 0.dp
         ),
         colors = ButtonDefaults.buttonColors(
-            containerColor = status.backgroundColor,
-            contentColor = status.contentColor
+            containerColor = status.backgroundColor(),
+            contentColor = status.contentColor()
         ),
-        content = content
+        shape = shape,
+        border = if (emptyHighlight) BorderStroke(1.5.dp, MaterialTheme.colorScheme.secondary) else null,
+        content = content,
+        modifier = modifier
+
     )
 }
