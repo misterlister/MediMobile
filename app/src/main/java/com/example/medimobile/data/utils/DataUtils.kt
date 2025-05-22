42f26a5e5f2be03961997e23d86f1eea1ede4a02
Hayden Lister <haydenglister@gmail.com>
2025-05-21 19:20:41 -0700

    Added NOT_SET to empty/null checker function

--- app/src/main/java/com/example/medimobile/data/utils/DataUtils.kt
+++ app/src/main/java/com/example/medimobile/data/utils/DataUtils.kt
@@ -1,9 +1,11 @@
 package com.example.medimobile.data.utils
 
+import com.example.medimobile.data.constants.DropdownConstants.NOT_SET
+
 
 fun isDataEmptyOrNull(value: Any?): Boolean {
     return when (value) {
-        is String -> value.isEmpty() // For String fields, check if empty
+        is String -> value.isEmpty() || value == NOT_SET // For String fields, check if empty/not set
         is Int -> value == 0 // For Int fields, check if zero (default value)
         else -> value == null // For any other type, check if null
     }
