7c1c0b125faba009f851601ee36905309d45b250
Hayden Lister <haydenglister@gmail.com>
2025-05-23 20:24:54 -0700

    Moved retrofit creation to network file

---
+++ app/src/main/java/com/example/medimobile/data/remote/network.kt
@@ -0,0 +1,36 @@
+package com.example.medimobile.data.remote
+
+import com.example.medimobile.data.constants.ApiConstants
+import com.example.medimobile.data.model.DropdownItem
+import com.example.medimobile.data.model.PatientEncounter
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.concurrent.TimeUnit
+
+
+fun createRetrofit(isLowConnectivity: Boolean, dropdownMappings: Map<String, List<DropdownItem>>): Retrofit {
+    val timeout = if (isLowConnectivity) ApiConstants.LOW_CONNECTIVITY_TIMEOUT else ApiConstants.REGULAR_TIMEOUT
+    val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(timeout, TimeUnit.SECONDS)
+        .readTimeout(timeout, TimeUnit.SECONDS)
+        .writeTimeout(timeout, TimeUnit.SECONDS)
+        .build()
+
+    val gson = GsonBuilder()
+        .registerTypeAdapter(LocalDate::class.java, LocalDateDeserializer())
+        .registerTypeAdapter(LocalTime::class.java, LocalTimeDeserializer())
+        .registerTypeAdapter(PatientEncounter::class.java, PatientEncounterDeserializer(dropdownMappings))
+        .serializeNulls()
+        .create()
+
+    return Retrofit.Builder()
+        .baseUrl(ApiConstants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(okHttpClient)
+        .build()
+}
+
