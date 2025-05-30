b0aef89e61fb6134a70f5bdbe15be327cfca4bc2
Hayden Lister <haydenglister@gmail.com>
2025-05-30 14:17:18 -0700

    Added navigation testing dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.androidx.navigation.testing)
+    androidTestImplementation(project(":app"))
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.navigation.compose)
