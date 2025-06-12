075132d08ba4c6e0435a8ccb3c8458f5fa804ba0
Hayden Lister <haydenglister@gmail.com>
2025-06-12 15:33:44 -0700

    Added test tags to ErrorPopup.

--- app/src/main/java/com/example/medimobile/ui/components/templates/ErrorPopup.kt
+++ app/src/main/java/com/example/medimobile/ui/components/templates/ErrorPopup.kt
@@ -3,21 +3,27 @@ package com.example.medimobile.ui.components.templates
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 @Composable
 fun ErrorPopup(
     errorTitle: String = "Error",
     errorMessage: String,
+    modifier: Modifier = Modifier,
     onDismiss: () -> Unit
 ) {
     AlertDialog(
         onDismissRequest = onDismiss,
         title = { Text(text = errorTitle) },
-        text = { Text(errorMessage) },
+        text = { Text(
+            text = errorMessage,
+            modifier = Modifier.testTag("errorPopupText")) },
         confirmButton = {
             MediButton(onClick = onDismiss) {
                 Text("Ok")
             }
-        }
+        },
+        modifier = modifier
     )
 }
