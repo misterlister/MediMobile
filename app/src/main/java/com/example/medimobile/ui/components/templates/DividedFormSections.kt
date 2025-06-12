11f077fa5c1d8da223b27cb91f245144dd2a728a
Hayden Lister <haydenglister@gmail.com>
2025-06-12 15:32:58 -0700

    Removed unused import

--- app/src/main/java/com/example/medimobile/ui/components/templates/DividedFormSections.kt
+++ app/src/main/java/com/example/medimobile/ui/components/templates/DividedFormSections.kt
@@ -3,7 +3,6 @@ package com.example.medimobile.ui.components.templates
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
