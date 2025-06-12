118845d739af1e24561ac0648c89dbcf065d5f8e
Hayden Lister <haydenglister@gmail.com>
2025-06-12 15:38:00 -0700

    Added test tags to Radio Buttons

--- app/src/main/java/com/example/medimobile/ui/components/templates/RadioButtonWithText.kt
+++ app/src/main/java/com/example/medimobile/ui/components/templates/RadioButtonWithText.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -31,7 +32,7 @@ fun RadioButtonWithText(
                     onOptionSelected(option)
                 }
             },
-            modifier = modifier
+            modifier = modifier.testTag("${option}RadioButton")
         )
         Text(text = option)
     }
