da14237eec1bcea13ebefcf845966e3d61ae058d
Hayden Lister <haydenglister@gmail.com>
2025-06-17 18:45:32 -0700

    Converted more plain text to constants

--- app/src/main/java/com/example/medimobile/data/constants/UIConstants.kt
+++ app/src/main/java/com/example/medimobile/data/constants/UIConstants.kt
@@ -3,4 +3,8 @@ package com.example.medimobile.data.constants
 object UIConstants {
     const val NO_USER = "Unknown User"
     const val NO_VISIT_ID = "Visit ID not set"
+    const val LOADING_MESSAGE = "Loading..."
+    const val NO_ENCOUNTERS_MESSAGE = "No encounters found in the selected range."
+    const val COMPLETE_ABBREVIATION = "Comp."
+    const val INCOMPLETE_ABBREVIATION = "Incomp."
 }
