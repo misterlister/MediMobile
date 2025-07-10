5f1398abfc7ee9da2a6655f4128de69c93288413
Hayden Lister <haydenglister@gmail.com>
2025-07-10 16:46:30 -0700

    Removed unused redundant Colors

--- app/src/main/java/com/example/medimobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/medimobile/ui/theme/Color.kt
@@ -2,39 +2,6 @@ package com.example.medimobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-
-/*
-val MediBlue = Color(0xFF1E80D9) // Blue colour to match Mediform's colour theme
-val MediRed = Color(0xFFCB2020) // Red colour to represent cancellation and errors
-val MediGreen = Color(0xFF1ACE1A) // Green colour to represent confirmation
-val MediYellow = Color(0xFFEFCF50) // Yellow colour to represent incomplete sections
-val MediGrey = Color(0xFFBBBBBB) // Grey colour for inactive buttons/menus
-
-val LightSecondaryBlue = Color(0xFF1674CB) // Darker blue to accentuate MediBlue in light mode
-val DarkSecondaryBlue = Color(0xFF329AF8) // Lighter blue to accentuate MediBlue in dark mode
-
-val LightBackground = Color(0xFFF0F9FF) // Almost white
-val LightVariant = Color(0xFFE4EEF7) // Very light blue
-val LightBanner = Color(0xFF9FB8D5) // Light blue/grey for light mode banners
-val LightContainer = Color(0xFFD1E1F5) // Light blue for light mode containers
-val SecondaryLightContainer = Color(0xFFDCE9FA) // Light blue for light mode focused containers
-
-val DarkBackground = Color(0xFF111B2D)  // Very dark gray
-val DarkVariant = Color(0xFF2A3B4D) // Very dark gray
-val DarkBanner = Color(0xFF34517F) // Dark blue/grey for dark mode banners
-val DarkContainer = Color(0xFF3A4E75) // Dark blue for dark mode containers
-val SecondaryDarkContainer = Color(0xFF4C5B84) // Dark blue for dark mode focused containers
-
-val TextOnBlue = Color.White // Text on a blue background
-val TextOnLight = Color.Black // Text on a light background
-val TextOnDark = Color.White // For text on a dark background
-val TextOnGrey = Color.White // Text on a grey background
-val TextOnRed = Color.White // Text on a red background
-val TextOnGreen = Color.White // Text on a green background
-// val TextOnYellow = Color.Black // Text on a yellow background
-*/
-
-
 val primaryLight = Color(0xFF005EA5)
 val onPrimaryLight = Color(0xFFFFFFFF)
 val primaryContainerLight = Color(0xFF0177CF)
