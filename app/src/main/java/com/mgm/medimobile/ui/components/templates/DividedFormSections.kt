93c125a8204e2b6e5c14544bb3ce0cc4cc86c6e4
Hayden Lister <haydenglister@gmail.com>
2025-07-22 14:08:45 -0700

    Reworked DividedFormSections to make it scrollable when content overflows

--- app/src/main/java/com/mgm/medimobile/ui/components/templates/DividedFormSections.kt
+++ app/src/main/java/com/mgm/medimobile/ui/components/templates/DividedFormSections.kt
@@ -1,8 +1,12 @@
 package com.mgm.medimobile.ui.components.templates
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -10,22 +14,29 @@ import com.mgm.medimobile.ui.components.HorizontalSectionDivider
 
 @Composable
 fun DividedFormSections(formSections: List<FormSectionData>) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        // Render each form section with horizontal dividers between them
-        formSections.forEachIndexed { index, section ->
-            FormSection(
-                title = section.title, // Title for the section
-                content = section.content, // Content to be displayed in the section
-                required = section.required
-            )
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            // Render each form section with horizontal dividers between them
+            formSections.forEachIndexed { index, section ->
+                FormSection(
+                    title = section.title, // Title for the section
+                    content = section.content, // Content to be displayed in the section
+                    required = section.required // Whether the section should show a star next to its title
+                )
 
-            // Add a horizontal divider between sections, but not after the last one
-            if (index < formSections.size - 1) {
-                HorizontalSectionDivider()
+                // Add a horizontal divider between sections, but not after the last one
+                if (index < formSections.size - 1) {
+                    HorizontalSectionDivider()
+                }
             }
         }
     }
