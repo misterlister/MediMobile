3c018fa99d1e6e3abc151a6557a6a5db936dcf22
misterlister <112644979+misterlister@users.noreply.github.com>
2025-07-24 19:42:21 -0700

    Added const string for timeout message

--- app/src/main/java/com/mgm/medimobile/data/constants/ApiConstants.kt
+++ app/src/main/java/com/mgm/medimobile/data/constants/ApiConstants.kt
@@ -4,4 +4,5 @@ object ApiConstants {
     const val BASE_URL = "https://cloudrun-service-api-fuau5e2ywq-pd.a.run.app/"
     const val REGULAR_TIMEOUT = 10L
     const val LOW_CONNECTIVITY_TIMEOUT = 30L
+    const val TIMEOUT_MESSAGE = "Your login session has timed out, please log back in"
 }
